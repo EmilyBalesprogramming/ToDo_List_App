a2b9c03aef1d174d27aa5d1b117c141553062370
Emily <emniba11@gmail.com>
2023-05-22 14:18:45 -0700

    added class "todo"

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@ function displayToDoItem(item) {
     var itemDate = document.createElement("p");
     itemDate.innerText = item.dueDate.toDateString();
     var itemDiv = document.createElement("div");
+    itemDiv.classList.add("todo");
     if (item.isCompleted) {
         itemDiv.classList.add("completed");
     }
